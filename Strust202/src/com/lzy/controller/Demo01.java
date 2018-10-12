253ab755fce567013a3432edc398f38b7f1da495
Administrator <1151025680@qq.com>
2018-10-12 22:49:24 +0800

    first git

---
+++ Strust202/src/com/lzy/controller/Demo01.java
@@ -0,0 +1,30 @@
+package com.lzy.controller;
+
+import com.opensymphony.xwork2.ActionContext;
+import com.opensymphony.xwork2.ActionSupport;
+
+import java.util.Arrays;
+import java.util.Map;
+
+/**
+ * Project: Strust2
+ * Package: com.lzy.controller
+ *
+ * @author : lzy
+ * @date : 2018/10/3 16:39
+ */
+public class Demo01 extends ActionSupport{
+
+    @Override
+    public String execute() throws Exception {
+        System.out.println("success");
+        ActionContext actionContext = ActionContext.getContext();
+        Map<String, Object> parameters = actionContext.getParameters();
+        for (String s : parameters.keySet()) {
+            String[] strings = (String[]) parameters.get(s);
+            System.out.println(s + "" + Arrays.toString(strings));
+        }
+        actionContext.put("pass","dwed ");
+        return SUCCESS;
+    }
+}
---
+++ Strust202/web/index.jsp
@@ -0,0 +1,20 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: Administrator
+  Date: 2018/10/3
+  Time: 23:09
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>$Title$</title>
+  </head>
+  <body>
+  $END$
+  <form action="/Demo01" method="post">
+    <input type="text" name="name">
+    <input type="submit">
+  </form>
+  </body>
+</html>
